09ffc616ec090d72c9d98d14fd609bb671e8b2dd
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-02-23 14:19:11 -0300

    Aula10_POF Parte2 (Utilizando Classes)

---
+++ Aula10_POF(Parte02)/Diretorio/Arquivo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diretorio
+{
+    internal class Arquivo
+    {
+        public static void Ler(int numeroArquivo)
+        {
+            string arquivoComCaminho = @"D:\Documentos\My_Projects\C#\Curso_C-\Aula07_FuncoesRecursivas\Arquivos\arq" + numeroArquivo + ".txt";
+            if (File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            Console.WriteLine("\n ======================================================================");
+            string arquivoComCaminho2 = @"D:\Documentos\My_Projects\C#\Curso_C-\Aula07_FuncoesRecursivas\Arquivos\arq" + (numeroArquivo + 1) + ".txt";
+            if (File.Exists(arquivoComCaminho2))
+            {
+                Arquivo.Ler(numeroArquivo + 1);
+            }
+        }
+    }
+}
---
+++ Aula10_POF(Parte02)/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Tela;
+
+namespace ProgramacaoFuncional
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Criar();
+        }
+    }
+}
