522b41092b3954dba8ada2713cac43d8662aac50
Gabriel Fernandes Alencar <gfalencar02@gmail.com>
2022-02-23 09:47:27 -0300

    Aula08_VariaveisConstantes

---
+++ Aula08_VariaveisConstantes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace VariaveisConstantes
+{
+    class program
+    {
+        public const int SAIDA_PROGRAMA = 1;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite " + SAIDA_PROGRAMA + " para sair do programa");
+                int valor = int.Parse(Console.ReadLine());
+
+                if (SAIDA_PROGRAMA == valor)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
